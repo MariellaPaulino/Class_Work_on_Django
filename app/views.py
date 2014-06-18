8d34519465fe645923dcf9fa01b1a58eeefb7e75
MariellaP123 <mmp344@gmail.com>
2014-06-18 10:31:25 -0400

    Render Templates

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from flask import render_template
 from app import app  
 
 @app.route('/')   				 
 def index():  
-	return ("Hello world!")	
+	return render_template('index.html')	
 
